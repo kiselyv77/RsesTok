67ef1f4d6900905e31a1e482e495cd5f8fb07143
Mi <vovo555vova444@gmail.com>
2021-11-09 22:07:31 +0300

    like_btn_on

---
+++ app/src/main/java/com/example/rsestok/ui/single_chat/message_recycling_view/view_holders/HolderVideoMessage.kt
@@ -0,0 +1,21 @@
+package com.example.rsestok.ui.single_chat.message_recycling_view.view_holders
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.rsestok.databinding.MessageItemTextBinding
+import com.example.rsestok.ui.single_chat.message_recycling_view.views.MessageView
+
+class HolderVideoMessage(itemMessage: MessageItemTextBinding): RecyclerView.ViewHolder(itemMessage.root), MessageHolder {
+
+    override fun drawMessage(view: MessageView) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onAttach(view: MessageView) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onDetach() {
+        TODO("Not yet implemented")
+    }
+
+}
